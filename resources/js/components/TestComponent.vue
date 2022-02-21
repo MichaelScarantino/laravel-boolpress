ddd0d55b72d57674f177c9618717eb7a8285755c
MichaelScarantino <scarantino.michael@outlook.it>
2022-02-21 15:45:10 +0100

    Create guest section and Vue.js

---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Sito in allestimento</h1>
+</template>
+
+<script>
+export default {
+    name: 'TestComponent'
+}
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <TestComponent />
+    </div>
+</template>
+
+<script>
+import TestComponent from '../components/TestComponent.vue';
+export default {
+   name: "App",
+   components: {
+       TestComponent
+   }
+}
+</script>
